872a3a2bb6bc0670dc17bd3a01a6b7d4ee306b02
Pranav Khurana <pranavkhurana24@gmail.com>
2023-08-03 16:30:33 +0530

    test commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-#Made by pranav Khurana
+# Made by pranav Khurana
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
