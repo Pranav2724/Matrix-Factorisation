17c93bce336782e45015be67dc0c996b1c3e6566
Pranav Khurana <pranavkhurana24@gmail.com>
2023-08-03 16:32:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # This is a sample Python script.
-# Made by pranav Khurana
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
